a720c7fad0610c07654050b4975440604125839e
progman <progman@ubuntu>
2014-03-08 16:38:17 +0800

    mod

--- adding/adding.c
+++ adding/adding.c
@@ -5,13 +5,17 @@
 void adding(uint64_t ret_addr);
 
 void adding(uint64_t ret_addr) {
-	*((uint64_t*) (sp - 8)) = bp;
-	bp = sp - 8;
-	sp = sp -0x10;
-	*((uint64_t*)(bp -8 )) = *((uint64_t*)(bp +0x18)) + *((uint64_t*)(bp+0x20));
-	*((uint64_t*)(bp + 0x10)) = *((uint64_t*)(bp-8));
-	sp = bp;
-	bp = *((uint64_t*)sp);
-	sp = sp + 8;
+	uint64_t local_sp = local_sp;
+	uint64_t local_bp = local_bp;
+	*((uint64_t*) (local_sp - 8)) = local_bp;
+	local_bp = local_sp - 8;
+	local_sp = local_sp -0x10;
+	*((uint64_t*)(local_bp -8 )) = *((uint64_t*)(local_bp +0x18)) + *((uint64_t*)(local_bp+0x20));
+	*((uint64_t*)(local_bp + 0x10)) = *((uint64_t*)(local_bp-8));
+	local_sp = local_bp;
+	local_bp = *((uint64_t*)local_sp);
+	local_sp = local_sp + 8;
+	bp = local_bp;
+	sp = local_sp;
 	return;
 }
