1e78591df96a66aa2accc36b2b6599bc4d377585
progman <progman@ubuntu>
2014-03-08 18:20:40 +0800

    mod

--- adding/adding.cpp
+++ adding/adding.cpp
@@ -4,12 +4,12 @@ void adding(uint64_t local_bp, uint64_t local_sp, uint64_t ret_addr);
 //register uint64_t local_bp __asm__ ("r12");
 //register uint64_t local_sp __asm__ ("r13");
 
-void adding(uint64_t local_bp, uint64_t local_sp, uint64_t ret_addr) {
+void adding(uint64_t local_sp, uint64_t ret_addr) {
 
 	//register uint64_t local_sp ;
 	//register uint64_t local_bp ;
-	*((uint64_t*) (local_sp - 8)) = local_bp;
-	local_bp = local_sp - 8;
+	//*((uint64_t*) (local_sp - 8)) = local_bp;
+	uint64_t local_bp = local_sp - 8;
 	local_sp = local_sp -0x10;
 	*((uint64_t*)(local_bp -8 )) = *((uint64_t*)(local_bp +0x18)) + *((uint64_t*)(local_bp+0x20));
 	*((uint64_t*)(local_bp + 0x10)) = *((uint64_t*)(local_bp-8));
