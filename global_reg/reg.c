64d61ae58d68c317d1a8ea37fec07651a5282391
progman <progman@ubuntu>
2014-03-09 13:59:29 +0800

    mod

--- global_reg/reg.c
+++ global_reg/reg.c
@@ -1,8 +1,19 @@
 #include <stdint.h>
 
 register uint64_t i0_BP __asm__ ("r12");
+register uint64_t i0_SP __asm__ ("r13");
 
 void test(void)
 {
 	i0_BP = 0;
+}
+
+void add(uint64_t ret_addr){
+	*((uint64_t*) (i0_SP - 8)) = i0_BP;
+	i0_BP = i0_SP -0x10;
+	*((uint64_t*)(i0_BP -8 )) = *((uint64_t*)(i0_BP +0x18)) + *((uint64_t*)(i0_BP+0x20));
+	*((uint64_t*)(i0_BP + 0x10)) = *((uint64_t*)(i0_BP-8));
+	i0_SP = i0_BP;
+	i0_BP = *((uint64_t*)i0_SP);
+	i0_SP = i0_SP + 8;
 }
